940733a0a3f1f259d697903d46605515f2699f79
Jhony Saenz <jhony.saenz.h@gmail.com>
2022-06-18 09:11:50 -0500

    Se agregaron archivos de python
    Agregue los archivos que estaban en el master anterior

---
+++ tk.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+# Definicion de la raiz
+root = Tk()
+
+#Atributos
+root.title("Titulo de la ventana")
+root.iconbitmap('hola.ico')
+root.resizable(True, True)
+
+# Bucle de la aplcacion
+root.mainloop()
+
