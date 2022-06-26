15aca5cf79f4eb03b9c2d9e68ffb22d07167c616
Jhony Saenz <jhony.saenz.h@gmail.com>
2022-06-25 20:53:42 -0500

    Clase 25 de Junio, creacion de un Menu
    En esta clase se puede gestionar un menu con sus respectivos sub menus

--- menu.py
+++ menu.py
@@ -8,9 +8,27 @@ menuBar = Menu(root)
 root.config(menu=menuBar)
 
 # Asigno las variables de los nombres a los menú
-archivoMenu = Menu(menuBar)
-editarMenu = Menu(menuBar)
-ayudaMenu = Menu(menuBar)
+archivoMenu = Menu(menuBar, tearoff=0)
+# Asigno los elementos (Submenu) al menu
+archivoMenu.add_command(label="Nuevo")
+archivoMenu.add_command(label="Abrir")
+archivoMenu.add_command(label="Guardar")
+archivoMenu.add_command(label="Cerrar")
+archivoMenu.add_separator()
+archivoMenu.add_command(label="Salir", command=root.quit)
+
+
+# Asigno las variables de los nombres a los menú
+editarMenu = Menu(menuBar, tearoff=0)
+editarMenu.add_command(label="Cortar")
+editarMenu.add_command(label="Copiar")
+editarMenu.add_command(label="Pegar")
+
+# Asigno las variables de los nombres a los menú
+ayudaMenu = Menu(menuBar, tearoff=0)
+ayudaMenu.add_command(label="Ayuda")
+ayudaMenu.add_separator()
+ayudaMenu.add_command(label="Acerca de...")
 
 # Dibujar o asigno los nombres en el menú con el metodo en cascada
 menuBar.add_cascade(label="Archivo", menu=archivoMenu)
