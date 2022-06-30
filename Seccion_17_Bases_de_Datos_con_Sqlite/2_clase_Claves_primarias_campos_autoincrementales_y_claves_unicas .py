be50a5fc7dfd64472fee70ee53ea4b7e7922ef9a
Jhony Saenz <jhony.saenz.h@gmail.com>
2022-06-29 20:57:19 -0500

    Clase 29/06/2022

--- Seccion_17_Bases_de_Datos_con_Sqlite/2_clase_Claves_primarias_campos_autoincrementales_y_claves_unicas .py
+++ Seccion_17_Bases_de_Datos_con_Sqlite/2_clase_Claves_primarias_campos_autoincrementales_y_claves_unicas .py
@@ -2,22 +2,19 @@
 import sqlite3
 
 # abro o creo la conexion
-conexion = sqlite3.connect("productos.db")
+conexion = sqlite3.connect("usuarios_autoincremental.db")
 
 # Inicializo el cursor
 cursor = conexion.cursor()
 
 # Sentencias
 
-# Creo la tabla
-cursor.execute('''
-CREATE TABLE productos(
-    id_producto INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre_producto VARCHAR (100) NOT NULL,
-    marca VARCHAR (50) NOT NULL,
-    precio FLOAT NOT NULL
-    )
-''')
+# Ingreso los registros
+
+# Hago consulta con el condicional WHERE (donde)
+cursor.execute("DELETE FROM usuarios WHERE dni='44444444D'")
+
+
 
 # Confirmo registro
 conexion.commit()
