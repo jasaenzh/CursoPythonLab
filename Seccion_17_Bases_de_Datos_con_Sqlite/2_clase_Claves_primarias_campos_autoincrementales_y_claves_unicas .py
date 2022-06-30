6a256d5221678f3ee5a77015fa9dfe2774a2221e
Jhony A Saenz <jash0310@gmail.com>
2022-06-29 19:03:07 -0500

    subir

--- Seccion_17_Bases_de_Datos_con_Sqlite/2_clase_Claves_primarias_campos_autoincrementales_y_claves_unicas .py
+++ Seccion_17_Bases_de_Datos_con_Sqlite/2_clase_Claves_primarias_campos_autoincrementales_y_claves_unicas .py
@@ -2,33 +2,22 @@
 import sqlite3
 
 # abro o creo la conexion
-conexion = sqlite3.connect("usuarios.db")
+conexion = sqlite3.connect("productos.db")
 
 # Inicializo el cursor
 cursor = conexion.cursor()
 
 # Sentencias
 
-## Creo Tabla
+# Creo la tabla
 cursor.execute('''
-CREATE TABLE usuarios
-    (
-        dni VARCHAR(9) PRIMARY KEY,
-        nombre VARCHAR(100),
-        edad INTEGER,
-        email VARCHAR(100)
+CREATE TABLE productos(
+    id_producto INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre_producto VARCHAR (100) NOT NULL,
+    marca VARCHAR (50) NOT NULL,
+    precio FLOAT NOT NULL
     )
 ''')
-
-# Insertar varios registros se deben hacer como tuplas
-usuarios = [
-    ('11111111A','Jhony Saenz',37,'jsaenz@correo.com'),
-    ('22222222B','Andres Ortiz',38,'andrescarlos@correo.com'),
-    ('33333333C','Luz Maria',45,'luzmaria@correo.com'),
-    ('44444444D','Jhony Saenz',37,'jsaenz@correo.com')
-]
-
-cursor.executemany("INSERT INTO usuarios VALUES(?,?,?,?)",usuarios)
 
 # Confirmo registro
 conexion.commit()
